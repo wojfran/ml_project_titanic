24d295c283e3acb2789cc391f5ea1a59547f9311
Franciszek Wójcik <f.wojcikk@gmail.com>
2024-03-14 10:15:06 +0900

    Almost there

--- data_manipulation.py
+++ data_manipulation.py
@@ -5,14 +5,20 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.naive_bayes import MultinomialNB
 
-def transform_input_data(set, training_set_num):
+def transform_input_data(set, training_set):
     set = set.drop(["PassengerId", "Name", "Cabin", "Ticket"], axis=1)
 
     set["Sex"] = np.where(set["Sex"] == "female", True, False)
     set["Survived"] = np.where(set["Survived"] == 1, True, False)
 
     set = set.dropna(subset=["Embarked"])
+    set = set.reset_index(drop=True)
 
+    training_set = training_set.dropna(subset=["Embarked"])
+    training_set = training_set.reset_index(drop=True)
+
+    training_set_num = training_set.drop(["PassengerId", "Survived", "Name", "Cabin", 
+                                      "Ticket", "Sex", "Embarked"], axis=1)
     set_num = set.drop(["Survived", "Embarked", "Sex"], axis=1)
 
     imputer = SimpleImputer(strategy="median")
@@ -24,17 +30,19 @@ def transform_input_data(set, training_set_num):
     scaler.fit(training_set_num)
     X = scaler.transform(set_num)
 
-    training_set_1hot = set[["Pclass", "SibSp", "Parch", "Embarked"]]
+    training_set_1hot = training_set[["Pclass", "SibSp", "Parch", "Embarked"]]
+    set_1hot = set[["Pclass", "SibSp", "Parch", "Embarked"]]
 
     cat_encoder = OneHotEncoder()
-    training_set_1hot = cat_encoder.fit_transform(training_set_1hot)
-
-    training_set_1hot = training_set_1hot.toarray()
-    training_set_1hot = pd.DataFrame(training_set_1hot, columns = cat_encoder.get_feature_names_out())
+    cat_encoder.fit_transform(training_set_1hot)
+    set_1hot = cat_encoder.transform(set_1hot)
+    set_1hot = set_1hot.toarray()
+    set_1hot = pd.DataFrame(set_1hot, columns = cat_encoder.get_feature_names_out())
 
     set_num = set_num.drop(["Pclass", "SibSp", "Parch"], axis=1)
 
     labels = set["Survived"]
-    set = pd.concat([set_num, set["Sex"], training_set_1hot], axis=1)
+
+    set = pd.concat([set_num, set["Sex"], set_1hot], axis=1)
 
     return set, labels
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sklearn.naive_bayes import MultinomialNB
 import data_manipulation as dm
 
 training_set_path = os.path.join(os.getcwd(), "datasets", "train.csv")
-training_set = pd.read_csv(training_set_path, index_col=False)
+training_set_original = pd.read_csv(training_set_path, index_col=False)
 
 test_set_path = os.path.join(os.getcwd(), "datasets", "test.csv")
 test_set = pd.read_csv(test_set_path, index_col=False)
@@ -15,13 +15,14 @@ test_set_labels = pd.read_csv(test_set_labels_path, index_col=False)
 
 test_set = pd.merge(test_set, test_set_labels, on='PassengerId')
 
-training_set_num = training_set.drop(["PassengerId", "Survived", "Name", "Cabin", 
-                                      "Ticket", "Sex", "Embarked"], axis=1)
+training_set, training_set_labels = dm.transform_input_data(training_set_original, training_set_original)
+test_set, test_set_labels = dm.transform_input_data(test_set, training_set_original)
 
-training_set, training_set_labels = dm.transform_input_data(training_set, training_set_num)
-test_set, test_set_labels = dm.transform_input_data(test_set, training_set_num)
+# print(training_set[training_set.isnull().any(axis=1)])
+# print(training_set.to_string())
 
-print(training_set[training_set.isnull().any(axis=1)])
+
+
 
 mnb = MultinomialNB().fit(training_set, training_set_labels)
 print("score on test: " + str(mnb.score(test_set, test_set_labels)))
