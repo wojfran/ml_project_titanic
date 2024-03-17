3713a8d9009ced71127d3b73e5bcc36a77217f0c
Franciszek Wójcik <f.wojcikk@gmail.com>
2024-03-17 20:10:05 +0900

    Finished project, will add a readme soon

--- data_manipulation.py
+++ data_manipulation.py
@@ -1,51 +1,109 @@
+from collections import defaultdict
 import numpy as np
 import pandas as pd
-from sklearn.discriminant_analysis import StandardScaler
-from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.naive_bayes import MultinomialNB
+from typing import Any, Tuple
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
 
-def transform_input_data(set, training_set):
-    set = set.drop(["PassengerId", "Name", "Cabin", "Ticket"], axis=1)
+# Function which extrapolates the amount of distnct titles
+# belonging to passengers in the array with their names
+# and returns a dictionary with the titles as keys and the
+# amount of passengers with that title as values
+def extract_titles(names):
+    titles = defaultdict(int)
+    for name in names:
+        title = name.split(",")[1].split(".")[0].strip()
+        titles[title] += 1
+    return titles
 
-    set["Sex"] = np.where(set["Sex"] == "female", True, False)
-    set["Survived"] = np.where(set["Survived"] == 1, True, False)
+# Function which extracts the titles from the names of the passengers]
+# and groups them into cathegories which would encapsulate all the titles
+# given that this is the total amount of each distinct title:
+# {'Mr': 517, 'Mrs': 125, 'Miss': 182, 'Master': 40, 'Don': 1, 'Rev': 6, 
+# 'Dr': 7, 'Mme': 1, 'Ms': 1, 'Major': 2, 'Lady': 1, 'Sir': 1, 'Mlle': 2, 
+#'Col': 2, 'Capt': 1, 'the Countess': 1, 'Jonkheer': 1}
+# The cathegories are: Common, Proffession and Child
+def group_title(name):
+    common = ["Mr", "Mrs", "Miss", "Ms", "Mme", "Mlle"]
+    profession = ["Don", "Rev", "Dr",  "Major", "Lady", "Sir", "Col", "Capt", "the Countess", "Jonkheer"]
+    child = ["Master"]
+    title = name.split(",")[1].split(".")[0].strip()
+    if title in common:
+        return "Common"
+    elif title in profession:
+        return "Profession"
+    elif title in child:
+        return "Child"
+    else:
+        return "Common"
+    
+# Function which groups groups the passenger by age into cathegories
+# The cathegories are: Child, Young, Adult and Elder, if age is null
+# the passenger is grouped as an Adult
+def group_age(age):
+    if age < 18:
+        return "Child"
+    elif age < 40:
+        return "Young"
+    elif age < 60:
+        return "Adult"
+    elif age >= 60:
+        return "Elder"
+    else:
+        return "Adult"
+    
+# Function which groups the pasenger based on the fare paid
+# The cathegories are: Low, Medium and High
+def group_fare(fare: float) -> str:
+    if fare < 10:
+        return "Low"
+    elif fare < 30:
+        return "Medium"
+    else:
+        return "High"
 
-    set = set.dropna(subset=["Embarked"])
-    set = set.reset_index(drop=True)
+def transform_input_data(data_set: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    print(data_set.info())
+    data_set = data_set.drop(["PassengerId", "Cabin", "Ticket"], axis=1)
+    data_set = data_set.dropna(subset=["Embarked"])
+    data_set = data_set.reset_index(drop=True)
 
-    training_set = training_set.dropna(subset=["Embarked"])
-    training_set = training_set.reset_index(drop=True)
+    data_set["Title"] = data_set["Name"].apply(group_title)
+    data_set = data_set.drop("Name", axis=1)
 
-    training_set_num = training_set.drop(["PassengerId", "Survived", "Name", "Cabin", 
-                                      "Ticket", "Sex", "Embarked"], axis=1)
-    set_num = set.drop(["Survived", "Embarked", "Sex"], axis=1)
+    data_set["Age"] = data_set["Age"].apply(group_age)
 
-    imputer = SimpleImputer(strategy="median")
-    imputer.fit(training_set_num)
-    X = imputer.transform(set_num)
-    set_num = pd.DataFrame(X, columns=set_num.columns, index=set_num.index)
+    data_set["Fare"] = data_set["Fare"].apply(group_fare)
 
-    scaler = StandardScaler()
-    scaler.fit(training_set_num)
-    X = scaler.transform(set_num)
+    data_set["Sex"] = np.where(data_set["Sex"] == "female", True, False)
+    data_set["Survived"] = np.where(data_set["Survived"] == 1, True, False)
 
-    training_set_1hot = training_set[["Pclass", "SibSp", "Parch", "Embarked"]]
-    set_1hot = set[["Pclass", "SibSp", "Parch", "Embarked"]]
-
-    print(set_1hot.head())
-    print(training_set_1hot.head())
+    dummy_values = [[0, 0, 0, 'C', 'Common', 'Adult', 'Low'],
+                    [1, 1, 1, 'Q', 'Common', 'Adult', 'Low'],
+                    [2, 2, 2, 'S', 'Common', 'Adult', 'Low'],
+                    [3, 3, 3, 'S', 'Common', 'Adult', 'Low'],
+                    [3, 4, 4, 'S', 'Common', 'Adult', 'Low'],
+                    [3, 5, 5, 'S', 'Common', 'Adult', 'Low'],
+                    [3, 6, 6, 'S', 'Common', 'Adult', 'Low'],
+                    [3, 7, 7, 'S', 'Common', 'Adult', 'Low'],
+                    [3, 8, 8, 'S', 'Common', 'Adult', 'Low'],
+                    [3, 8, 9, 'C', 'Common', 'Adult', 'Low']]
+    dummy_df = pd.DataFrame(dummy_values, columns=["Pclass", "SibSp", "Parch", "Embarked", "Title", "Age", "Fare"])
+    data_set_1hot = data_set[["Pclass", "SibSp", "Parch", "Embarked", "Title", "Age", "Fare"]]
+    dummy_1hot = pd.concat([dummy_df, data_set_1hot], axis=0, ignore_index=True)
 
     cat_encoder = OneHotEncoder(handle_unknown='ignore')
-    cat_encoder.fit_transform(training_set_1hot)
-    set_1hot = cat_encoder.transform(set_1hot)
-    set_1hot = set_1hot.toarray()
-    set_1hot = pd.DataFrame(set_1hot, columns = cat_encoder.get_feature_names_out())
+    cat_encoder.fit(dummy_1hot)  
+    data_set_1hot = cat_encoder.transform(data_set_1hot)
+    data_set_1hot = data_set_1hot.toarray()
+    data_set_1hot = pd.DataFrame(data_set_1hot, columns = cat_encoder.get_feature_names_out())
 
-    set_num = set_num.drop(["Pclass", "SibSp", "Parch"], axis=1)
+    labels = data_set["Survived"]
 
-    labels = set["Survived"]
+    data_set = data_set.drop(["Pclass", "SibSp", "Parch", "Embarked", "Title", "Age", "Fare", "Survived"], axis=1)
 
-    set = pd.concat([set_num, set["Sex"], set_1hot], axis=1)
+    data_set = pd.concat([data_set, data_set_1hot], axis=1)
 
-    return set, labels
+    return data_set, labels
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import cross_val_score
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
 import data_manipulation as dm
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
 training_set_path = os.path.join(os.getcwd(), "datasets", "train.csv")
-training_set_original = pd.read_csv(training_set_path, index_col=False)
+training_set = pd.read_csv(training_set_path, index_col=False)
 
 test_set_path = os.path.join(os.getcwd(), "datasets", "test.csv")
 test_set = pd.read_csv(test_set_path, index_col=False)
@@ -15,15 +19,51 @@ test_set_labels = pd.read_csv(test_set_labels_path, index_col=False)
 
 test_set = pd.merge(test_set, test_set_labels, on='PassengerId')
 
-training_set, training_set_labels = dm.transform_input_data(training_set_original, training_set_original)
-test_set, test_set_labels = dm.transform_input_data(test_set, training_set_original)
+training_set, training_set_labels = dm.transform_input_data(training_set)
+test_set, test_set_labels = dm.transform_input_data(test_set)
 
-# print(training_set[training_set.isnull().any(axis=1)])
-# print(training_set.to_string())
+mnb = MultinomialNB()
+log_reg = LogisticRegression(max_iter=1000)
+svc = SVC()
+rfc = RandomForestClassifier()
 
+mnb_score = cross_val_score(mnb, training_set, training_set_labels, cv=10, scoring="accuracy")
+log_reg_score = cross_val_score(log_reg, training_set, training_set_labels, cv=10, scoring="accuracy")
+svc_score = cross_val_score(svc, training_set, training_set_labels, cv=10, scoring="accuracy")
+rfc_score = cross_val_score(rfc, training_set, training_set_labels, cv=10, scoring="accuracy")
 
+mnb.fit(training_set, training_set_labels)
+log_reg.fit(training_set, training_set_labels)
+svc.fit(training_set, training_set_labels)
+rfc.fit(training_set, training_set_labels)
 
+def display_scores(scores):
+    print("Scores:", scores)
+    print("Mean:", scores.mean())
+    print("Standard deviation:", scores.std())
+    print("\n")
 
-mnb = MultinomialNB().fit(training_set, training_set_labels)
-print("score on test: " + str(mnb.score(test_set, test_set_labels)))
-print("score on test: " + str(mnb.score(training_set, training_set_labels)))
+print("MultinomialNB")
+display_scores(mnb_score)
+print("Logistic Regression")
+display_scores(log_reg_score)
+print("SVC")
+display_scores(svc_score)
+print("Random Forest")
+display_scores(rfc_score)
+
+mnb_predictions = mnb.predict(test_set)
+log_reg_predictions = log_reg.predict(test_set)
+svc_predictions = svc.predict(test_set)
+rfc_predictions = rfc.predict(test_set)
+
+mnb_accuracy = np.mean(mnb_predictions == test_set_labels)
+log_reg_accuracy = np.mean(log_reg_predictions == test_set_labels)
+svc_accuracy = np.mean(svc_predictions == test_set_labels)
+rfc_accuracy = np.mean(rfc_predictions == test_set_labels)
+
+print("MultinomialNB accuracy:", mnb_accuracy)
+print("Logistic Regression accuracy:", log_reg_accuracy)
+print("SVC accuracy:", svc_accuracy)
+print("Random Forest accuracy:", rfc_accuracy)
+
